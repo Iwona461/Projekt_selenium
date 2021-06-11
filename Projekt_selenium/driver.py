3bd22351b2aee73430df3da4bed84a9927a35f14
Ryszard Bryłowski <Iwona461@users.noreply.github.com>
2021-06-11 23:54:16 +0200

    chrome

--- Projekt_selenium/driver.py
+++ Projekt_selenium/driver.py
@@ -1,8 +1,12 @@
+import time
 from selenium import webdriver
-from selenium.webdriver import Firefox
 
-driver = Firefox()
-from selenium.webdriver import Firefox
 
-with Firefox() as driver:
-Firefox(executable_path='/path/to/geckodriver')
+driver = webdriver.Chrome('/path/to/chromedriver')  # Optional argument, if not specified will search path.
+driver.get('http://www.google.com/');
+time.sleep(5) # Let the user actually see something!
+search_box = driver.find_element_by_name('q')
+search_box.send_keys('ChromeDriver')
+search_box.submit()
+time.sleep(5) # Let the user actually see something!
+driver.quit()
